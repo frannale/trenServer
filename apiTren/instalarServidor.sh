4b887b6928e203803a268b857bae76130ae68f39
Alechuu <alejo9726@gmail.com>
2022-12-12 20:42:10 -0300

    Instalador actualizado

--- apiTren/instalarServidor.sh
+++ apiTren/instalarServidor.sh
@@ -5,6 +5,6 @@ python3 -m venv venv
 echo 'Activando virtual environment...'
 source venv/bin/activate
 echo 'Instalando todo lo necesario...'
-pip install -r requirements.txt
+pip install -r requirements.txt --no-index --find-links file:./Dependencies
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo 'Listo! Para correr el servidor, ejecuta el archivo correrServidor.sh'
